18ae27b97118a9389d6f0aa6b0ec668021da3e82
Alexis <alexiscristiano2004@gmail.com>
2024-12-13 16:56:18 +0100

    Site Post fini (category, post, show)

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -22,4 +22,32 @@ class PostController extends AbstractController
             'posts' => $posts
         ]);
     }
+
+     /**
+     * @Route("/post/{id}", name="app_post_show")
+     */
+    public function show($id)
+    {
+        include (__DIR__.'/../../data/posts.php');
+
+        $posts = json_decode($posts);
+        dump($id);
+        return $this->render('post/show.html.twig', [
+            'posts' => $posts , "id" => $id
+         ]);
+    }
+
+    /**
+     * @Route("/category/{id}", name="app_post_category")
+     */
+    public function category($id)
+    {
+        include (__DIR__.'/../../data/posts.php');
+
+        $posts = json_decode($posts);
+        dump($id);
+        return $this->render('post/category.html.twig', [
+            'posts' => $posts , "idtag" => $id
+         ]);
+    }
 }
