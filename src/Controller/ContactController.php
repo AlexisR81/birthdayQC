ca32668505e76d89aba6e75d838940f5c1a17c33
Alexis <alexiscristiano2004@gmail.com>
2024-12-13 16:20:07 +0100

    Commit posts

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -3,7 +3,9 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\BrowserKit\Response;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response as HttpFoundationResponse;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ContactController extends AbstractController
@@ -11,11 +13,11 @@ class ContactController extends AbstractController
     /**
      * @Route("/contact", name="app_contact")
      */
-    public function index(): JsonResponse
+    public function index(): HttpFoundationResponse
     {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/ContactController.php',
-        ]);
+        return $this->render(
+            'contact/index.html.twig',
+            ['firstname'=>'Alexis']
+        );
     }
 }
